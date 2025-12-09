478ef154701814fad9c7d9d821c6e9af811268e8
wuw-shz <wuw62481@gmail.com>
2025-12-10 03:14:22 +0700

    @wuw-shz 10/12/25 x21

--- src/listeners/mouse/x1.ts
+++ src/listeners/mouse/x1.ts
@@ -1,20 +1,30 @@
-import { keyboard } from "winput";
-import { gameStates } from "../../states";
+import { keyboard, mouse } from "winput";
+import { gameStates, robloxStates } from "../../states";
 import { createHandler } from "../types";
-import { withLock } from "../utils";
+import { waitFor, withLock } from "../utils";
+
+function shouldAbort(): boolean {
+  return (
+    !robloxStates.get("is_active") ||
+    gameStates.get("is_toss") ||
+    mouse.isPressed("x1")
+  );
+}
 
 export default createHandler("x1", {
   down: async () => {
     await withLock("x1", async () => {
       if (gameStates.get("is_toss")) return;
-      keyboard.tap("space");
-      await keyboard.waitForRelease("space");
+      keyboard.press("space");
+      await waitFor(() => !gameStates.get("is_on_ground"), shouldAbort);
+      keyboard.release("space");
       keyboard.press("e");
     });
   },
   up: async () => {
     await withLock("x1", async () => {
       if (gameStates.get("is_toss")) return;
+      await waitFor(() => !gameStates.get("is_on_ground"), shouldAbort);
       keyboard.release("e");
     });
   },
